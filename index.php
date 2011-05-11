db38e201552eaa4753424048efb376254f9aca11
simondelliott <simondelliott@gmail.com>
2011-05-11 13:37:45 +0100

    fixed the index.php

--- index.php
+++ index.php
@@ -1,77 +1,33 @@
-# Protect files and directories from prying eyes.
-#<FilesMatch "\.(engine|inc|info|install|module|profile|test|po|sh|.*sql|theme|tpl(\.php)?|xtmpl|svn-base)$|^(code-style\.pl|Entries.*|Repository|Root|Tag|Template|all-wcprops|entries|format)$">
-#  Order allow,deny
-#</FilesMatch>
+<?php
 
-# Follow symbolic links in this directory.
-Options +FollowSymLinks
-Options -Indexes
+require_once ( 'lib/framework/common.php');
+require_once ( 'routes.php');
 
-# Make Drupal handle any 404 errors.
-ErrorDocument 404 /index.php
+$selected_route = "";
+$route = null;
 
-# Force simple error message for requests for non-existent favicon.ico.
-<Files favicon.ico>
-  # There is no end quote below, for compatibility with Apache 1.3.
-  ErrorDocument 404 "The requested file favicon.ico was not found.
-</Files>
+foreach ($routes as $name => $r){
+    $selected_route = $name;
+    if ($r->matches()){
+        $route = $r;
+        break;
+    }
+    if ($name == "root"){
+        $root_route = $r;
+    }
+}
 
-# Set the default handler.
-DirectoryIndex index.php
+if ($route ==NULL)
+    $route = $root_route; //note could still be null
 
-# Override PHP settings. More in sites/default/settings.php
-# but the following cannot be changed at runtime.
+if ($route ==NULL){
+    handle_error("No route to follow");
+}
 
-# PHP 4, Apache 1.
-<IfModule mod_php4.c>
-  php_value magic_quotes_gpc                0
-  php_value register_globals                0
-  php_value session.auto_start              0
-  php_value mbstring.http_input             pass
-  php_value mbstring.http_output            pass
-  php_value mbstring.encoding_translation   0
-</IfModule>
+$GLOBALS["route"] = $route;
+$controler = $route->get_controler_class();
+$action = $route->action;
 
-# PHP 4, Apache 2.
-<IfModule sapi_apache2.c>
-  php_value magic_quotes_gpc                0
-  php_value register_globals                0
-  php_value session.auto_start              0
-  php_value mbstring.http_input             pass
-  php_value mbstring.http_output            pass
-  php_value mbstring.encoding_translation   0
-</IfModule>
+$controler = new $controler($action, $route->matches);
 
-# PHP 5, Apache 1 and 2.
-<IfModule mod_php5.c>
-  php_value magic_quotes_gpc                0
-  php_value register_globals                0
-  php_value session.auto_start              0
-  php_value mbstring.http_input             pass
-  php_value mbstring.http_output            pass
-  php_value mbstring.encoding_translation   0
-</IfModule>
-
-# Requires mod_expires to be enabled.
-<IfModule mod_expires.c>
-  # Enable expirations.
-  ExpiresActive On
-
-  # Cache all files for 2 weeks after access (A).
-  ExpiresDefault A1209600
-
-  # Do not cache dynamically generated pages.
-  ExpiresByType text/html A1
-</IfModule>
-
-<IfModule mod_rewrite.c>
-  RewriteEngine on
-
-  RewriteRule ^terms_and_conditions$ /terms_and_conditions.php [L]
-  RewriteRule ^nyc$ /nyc/test.php [L]
-  RewriteBase /
-  RewriteCond %{REQUEST_FILENAME} !-f
-  RewriteCond %{REQUEST_FILENAME} !-d
-  RewriteRule . /index.php [L]
-
-</IfModule> 
+?>
